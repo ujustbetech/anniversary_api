67b127db59d8b0dcb5485c647aa069791f4f239a
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2023-02-15 18:44:10 +0530

    error state update

--- partner_app/views.py
+++ partner_app/views.py
@@ -31,7 +31,7 @@ def Registration(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response( status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
 @api_view(['GET', 'PUT', 'DELETE'])
 def partnerlogin(request,pk):
